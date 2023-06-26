1a3fd459e62ac45f4ce2d9f68cd446035c7c2146
Saiteja on work <137623367+SaitejaChakravarthula-Work@users.noreply.github.com>
2023-06-26 20:14:06 +0530

    Update Main.java

--- JAVA/DAY-4/src/Main.java
+++ JAVA/DAY-4/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        /*
+        
         System.out.println("Enter the MARKS obtained : ");
 
         Scanner s =new Scanner(System.in);
@@ -25,6 +25,6 @@ public class Main {
             System.out.println("U failed");
         }
 
-         */
+         
     }
 }
